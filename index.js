af932a6c4785fa730e0ed694c7520d9c9915dc8f
Christian Deverall <christiandeverall1@gmail.com>
2018-09-01 14:16:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
-const API_KEY = "YOUR API KEY"
+const API_KEY = "314730d255fa1dffba8148ad3661cfde"
 
 function handleFormSubmit(event) {
-  //handle submit event
+    event.preventDefault();
+    let cityName=document.getElementById("city").value;
+    fetchCurrentWeather(cityName);
+    fetchFiveDayForecast(cityName);
 }
 
 function fetchCurrentWeather(city) {
-  //fetch current weather based on city
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`)
+    .then(res=>res.json())
+    .then(data=>displayCurrentWeather(data))
+    .catch(error=>console.log(error))
 }
 
 function displayCurrentWeather(json) {
   //render current weather data to the DOM using provided IDs and json from API
+  console.log(json);
+  document.getElementById("temp").innerHTML=json["main"]["temp"];
+  document.getElementById("low").innerHTML=json["main"]["temp_min"];
+  document.getElementById("high").innerHTML=json["main"]["temp_max"];
+  document.getElementById("humidity").innerHTML=json["main"]["humidity"];
+  document.getElementById("cloudCover").innerHTML=json["clouds"]["all"];
+  document.getElementById("sunrise").innerHTML=json["sys"]["sunrise"];
+  document.getElementById("sunset").innerHTML=json["sys"]["sunset"];
 }
 
 
 function fetchFiveDayForecast(city) {
-  //fetch five day forecast data based on city
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${API_KEY}`)
+  .then(res=>res.json())
+  .then(json=>displayFiveDayForecast(json))
+  .catch(error=>console.log(error))
 }
 
 function displayFiveDayForecast(json) {
   //render five day forecast data to the DOM using provided IDs and json from API
+  let aside=document.getElementById("fiveDayForecast");
+  aside.innerHTML="";
+  for(let i=7;i<=39;i+=8){
+    let temp_child=document.createElement("div");
+    temp_child.innerHTML=`Time: ${json["list"][i]["dt_txt"]}`;
+    temp_child.innerHTML+=` Temperature: ${json["list"][i]["main"]["temp"]}`
+    temp_child.innerHTML+=` Humidity: ${json["list"][i]["main"]["humidity"]}`
+    aside.appendChild(temp_child);
+  }
 }
 
 function createChart(json) {
@@ -26,5 +52,6 @@ function createChart(json) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  //add event listener here for form submission
-})
+  document.getElementById("submit1").addEventListener("click", handleFormSubmit);
+});
+
